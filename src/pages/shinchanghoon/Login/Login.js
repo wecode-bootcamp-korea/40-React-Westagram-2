63048481af5228cc113ac146508ef52391f3d738
unknown <shinch8803@gmail.com>
2022-11-24 14:53:19 +0900

    Add: Changhoon 초기 위스타그램~

--- src/pages/shinchanghoon/Login/Login.js
+++ src/pages/shinchanghoon/Login/Login.js
@@ -1,7 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css';
 
-const LoginChangHoon = () => {
-  return <div>신창훈 로그인 창</div>;
-};
+function Login() {
+  return (
+    <div class="layout">
+      <img src=".\images\shinchanghoon\westagram.png" />
+      <div class="login-container">
+        <input
+          type="text"
+          id="login_id"
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+        />
+        <input type="password" id="login_pw" placeholder="비밀번호" />
+        <button class="login-btn" id="login_btn">
+          로그인
+        </button>
+        <p class="alert" />
 
-export default LoginChangHoon;
+        <div class="pwComment">비밀번호를 잊으셨나요?</div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
+
+// const idInput = document.getElementById('login_id');
+// const pwInput = document.getElementById('login_pw');
+// const loginBtn = document.getElementById('login_btn');
+
+// const isActiveLogin = () => {
+//   let idValue = idInput.value;
+//   let pwValue = pwInput.value;
+
+//   if (pwValue.length > 2 && idValue.length > 2) {
+//     loginBtn.classList.add('login_active');
+//   } else {
+//     loginBtn.classList.remove('login_active');
+//   }
+// };
+
+// idInput.addEventListener('keyup', isActiveLogin);
+// pwInput.addEventListener('keyup', isActiveLogin);
--- src/pages/shinchanghoon/Main/Main.js
+++ src/pages/shinchanghoon/Main/Main.js
@@ -1,7 +1,93 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Main.css';
 
-const MainChangHoon = () => {
-  return <div>신창훈 메인 창</div>;
-};
+function Main() {
+  return (
+    <>
+      <div class="header">
+        <img class="instagram" src=".\images\shinchanghoon\instagram.png" />
+        <img class="westagram" src=".\images\shinchanghoon\westagram.png" />
+        <div class="input-wrapper">
+          <input type="text" aria-label="검색창" />
+          <div class="icon-wrapper">
+            <i class="fas fa-keyboard" aria-label="입력 도구" />
+            <i class="fas fa-microphone" aria-label="음성 검색" />
+          </div>
+        </div>
+        <img class="explore" src=".\images\shinchanghoon\explore.png" />
+        <img class="heart" src=".\images\shinchanghoon\heart.png" />
+        <img class="profile" src=".\images\shinchanghoon\profile.png" />
+      </div>
+      <div class="main">
+        <div class="center">
+          {/* <!--사진좋아요넣을부분--> */}
+          <div class="article">
+            <div class="profilephoto">
+              <img
+                class="circle_photo"
+                src=".\images\shinchanghoon\circle_photo.jpg"
+              />
+            </div>
 
-export default MainChangHoon;
+            <div class="nameandprofile">
+              <li>Shinchanghoon</li>
+              <li>Frontend Developer</li>
+            </div>
+
+            <div class="centerarticle">
+              <img
+                class="center_photo"
+                src=".\images\shinchanghoon\main_sea.jpg"
+              />
+            </div>
+
+            <div class="follow_icon">
+              <img
+                class="small_icon heart2"
+                src=".\images\shinchanghoon\heart.png"
+              />
+              <img
+                class="small_icon"
+                src=".\images\shinchanghoon\follows.png"
+              />
+              <img
+                class="small_icon"
+                src=".\images\shinchanghoon\comment.png"
+              />
+
+              <img
+                class="small_icon flight"
+                src=".\images\shinchanghoon\flight.png"
+              />
+            </div>
+
+            <div class="like">
+              <p class="bold">username</p>
+              <p class="normal">님 외 10명이 좋아합니다.</p>
+            </div>
+
+            <div class="commnet">
+              <p class="bold">username</p>
+
+              <p class="normal">어디 바닷가에서 찍었냐?</p>
+            </div>
+            {/* <!--댓글기능 추가로 손봐야됨--> */}
+            <div id="form-commentInfo">
+              <div id="comment-count">
+                댓글 <span id="count">1</span>
+              </div>
+              <input id="comment-input" placeholder="댓글을 입력해 주세요." />
+              <button id="submit">등록</button>
+            </div>
+          </div>
+        </div>
+
+        {/* <!--오른쪽 팔로워들--> */}
+        <div class="right" />
+      </div>
+    </>
+  );
+}
+
+export default Main;
