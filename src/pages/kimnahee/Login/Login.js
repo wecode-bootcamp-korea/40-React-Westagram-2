cdd3cabecc703847e4ba5a6a2b6869772b6c6702
NA <na@NAui-MacBookAir.local>
2022-11-26 19:57:36 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/kimnahee/Login/Login.js
+++ src/pages/kimnahee/Login/Login.js
@@ -7,12 +7,27 @@ const LoginNaHee = () => {
 
   const [idValue, setIdValue] = useState('');
   const [pwdValue, setPwdValue] = useState('');
+
+  const [active, setActive] = useState(true);
+  const [className, setClassName] = useState('loginBtn');
+
   const saveUserId = e => {
     setIdValue(e.target.value);
   };
   const saveUserPwd = e => {
     setPwdValue(e.target.value);
   };
+  const setAll = (act, Name) => {
+    setActive(act);
+    setClassName(Name);
+  };
+
+  const valid = () => {
+    return idValue.includes('@') && pwdValue.length >= 5
+      ? setAll(false, 'loginBtnActive')
+      : setAll(true, 'loginBtn');
+  };
+
   const goToMain = () => {
     navigate('/MainNaHee');
   };
@@ -24,15 +39,19 @@ const LoginNaHee = () => {
         className="idBox"
         type="text"
         placeholder="전화번호, 사용자 이름 또는 이메일"
+        onKeyUp={valid}
         onChange={saveUserId}
       />
       <input
         className="pwdBox"
         placeholder="비밀번호"
         type="password"
+        onKeyUp={valid}
         onChange={saveUserPwd}
       />
-      <button onClick={goToMain}>로그인</button>
+      <button className={className} disabled={active} onClick={goToMain}>
+        로그인
+      </button>
       <div className="forgetPassword">
         <a className="passwordLink" href="####">
           비밀번호를 잊으셨나요?
