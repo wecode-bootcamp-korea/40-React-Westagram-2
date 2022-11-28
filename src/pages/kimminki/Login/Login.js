bb5d8502a3bdb9fa0f969efa8222b7d3c2e4de60
minki3 <86992006+minki3@users.noreply.github.com>
2022-11-28 11:28:40 +0900

    완성

--- src/pages/kimminki/Login/Login.js
+++ src/pages/kimminki/Login/Login.js
@@ -1,7 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../Login/Login.scss';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-const LoginMinKi = () => {
-  return <div>김민기 로그인 창</div>;
+const LoginPage = () => {
+  const [id, setId] = useState('');
+  const [pass, setPassword] = useState('');
+  const [color, setColor] = useState('blue');
+  const [button, setButton] = useState(true);
+
+  const saveUserId = event => {
+    setId(event.target.value);
+    checkLogin();
+  };
+  const savePassword = event => {
+    setPassword(event.target.value);
+    checkLogin();
+  };
+
+  const checkLogin = () => {
+    if (id.includes('@') && pass.length > 5) {
+      setColor('skyblue');
+      setButton(false);
+    }
+  };
+
+  const Navigate = useNavigate();
+
+  const goToMain = () => {
+    Navigate('/main');
+  };
+  return (
+    <div className="login-display">
+      <div className="login-box">
+        <div className="logo">westagram</div>
+        <div className="check-box">
+          <input
+            className="idpas"
+            type="text"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+            value={id}
+            onChange={saveUserId}
+          />
+          <input
+            className="idpas"
+            type="password"
+            placeholder="비밀번호"
+            value={pass}
+            onChange={savePassword}
+          />
+          <button
+            style={{ backgroundColor: color }}
+            className="check"
+            onClick={goToMain}
+            disabled={button}
+          >
+            로그인
+          </button>
+          {/* disabled */}
+          <Link to="/main">main페이지로 이동</Link>
+          <a href="#" className="forget">
+            비밀번호를 잊으셨나요?
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default LoginMinKi;
+export default LoginPage;
--- src/pages/kimminki/Main/Main.js
+++ src/pages/kimminki/Main/Main.js
@@ -1,7 +1,152 @@
 import React from 'react';
+import '../Main/Main.scss';
+import { useState } from 'react';
+import CommentArea from './comment';
+const MainPage = () => {
+  const [comment, setComment] = useState([]);
+  const [commentList, setCommentList] = useState([]);
 
-const MainMinKi = () => {
-  return <div>김민기 메인 창</div>;
+  const commentValue = event => {
+    setComment(event.target.value);
+  };
+
+  const addComment = () => {
+    setCommentList(commentList.concat(comment));
+    setComment('');
+  };
+
+  const commentArea = commentList.map((element, i) => (
+    <span key={i}>
+      <li>{element}</li>
+    </span>
+  ));
+  return (
+    <div className="main-display">
+      <nav className="navi">
+        <div className="left-top">
+          <i className="fa-solid fa-camera camera" />
+          <span className="searchLogo">westagram</span>
+        </div>
+        <div className="search-bar">
+          <input className="search" placeholder="검색" type="text" />
+          <i className="fa-solid fa-magnifying-glass glass" />
+        </div>
+        <div className="right-logo">
+          <div className="compass" />
+          <div className="heart" />
+          <div className="mypage" />
+        </div>
+      </nav>
+
+      <main>
+        <div className="feeds">
+          <div className="profile">
+            <img className="user-img" />
+            <div className="user-name"> min__ki</div>
+            <i className="fa-solid fa-bars bar" />
+          </div>
+          <article className="main-page" />
+          <div className="content-bar">
+            <i className="fa-regular fa-heart fa-2x like" />
+            <i className="fa-regular fa-comment fa-2x spech" />
+            <i className="fa-solid fa-arrow-up-from-bracket fa-2x arrow" />
+          </div>
+          <div className="likey">
+            <img className="people" />
+            <div className="like-people"> ooo님외 o명이 좋아합니다.</div>
+          </div>
+          <ul className="comment-area">
+            <CommentArea comment={commentArea} />
+          </ul>
+          <div className="comment-Input">
+            <input
+              className="comment-text"
+              type="text"
+              placeholder="댓글달기..."
+              value={comment}
+              onChange={commentValue}
+            />
+            <button className="registration" onClick={addComment}>
+              게시
+            </button>
+          </div>
+        </div>
+        <div className="main-right">
+          <div className="right-1">
+            <img className="profile2" />
+            <div className="right-font1">wecode_bootcamp</div>
+            <div className="right-font2">wecode | 위코드</div>
+          </div>
+          <div className="right-2">
+            <div className="right-op">
+              <div className="story right-font2">스토리</div>
+              <div className="all">모두보기</div>
+            </div>
+            <div className="boundary">
+              <img className="profile2" />
+              <div className="right-font1">abcdefg</div>
+              <div className="right-font2">1 분전</div>
+            </div>
+            <div className="boundary">
+              <img className="profile2" />
+              <div className="right-font1">abcdefg</div>
+              <div className="right-font2">1 분전</div>
+            </div>
+            <div className="boundary">
+              <img className="profile2" />
+              <div className="right-font1">abcdefg</div>
+              <div className="right-font2">1 분전</div>
+            </div>
+            <div className="boundary">
+              <img className="profile2" />
+              <div className="right-font1">abcdefg</div>
+              <div className="right-font2">1 분전</div>
+            </div>
+            <div className="boundary">
+              <img className="profile2" />
+              <div className="right-font1">abcdefg</div>
+              <div className="right-font2">1 분전</div>
+            </div>
+          </div>
+          <div className="right-3">
+            <div className="right-op">
+              <div className="story right-font2">회원님을 위한 추천</div>
+              <div className="all">모두 보기</div>
+            </div>
+            <div className="boundary">
+              <img className="profile2" />
+              <div className="right-font1">abcdefg</div>
+              <div className="right-font2">1 분전</div>
+              <div className="follow">팔로우</div>
+            </div>
+            <div className="boundary">
+              <img className="profile2" />
+              <div className="right-font1">abcdefg</div>
+              <div className="right-font2">1 분전</div>
+              <div className="follow">팔로우</div>
+            </div>
+            <div className="boundary">
+              <img className="profile2" />
+              <div className="right-font1">abcdefg</div>
+              <div className="right-font2">1 분전</div>
+              <div className="follow">팔로우</div>
+            </div>
+          </div>
+          <div className="info">
+            <p>
+              instagram정보 지원 홍보 센터 API <br />
+              채용 정보 개인정보처리방침 약관
+              <br />
+              디렉터리 프로필 해시태그 언어
+              <br />
+              <br />
+              <br />@ 2019 INSTAGRAM
+            </p>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 };
 
-export default MainMinKi;
+export default MainPage;
